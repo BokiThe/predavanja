4a4cbd6dcb071dc84296a177fba898b79ecd6c0f
BokiThe <ristic1989@gmail.com>
2021-11-26 17:08:27 +0100

    dodaati zadaci.js  u osnove js

---
+++ 09_JS_OOSNOVE/script.js
@@ -0,0 +1,74 @@
+"Use strict";
+
+console.log("Pozdrav iz JavaScript datoteke");
+
+let a  = 3, b = 6;
+console.log(a * b);
+
+console.log("Pera" + " Peric");
+console.log("Pera" * " Peric");
+
+b = b/a;
+ 
+console.log(a,b);
+
+a = b * a + a;
+console.log(a,b)// a = 9, b = 2
+
+b = (a - 2*b)/b;
+
+console.log(a,b);
+
+//a = a + 10; 
+a+=10;
+console.log(a);
+
+//a = a + 1;
+//a+=1;
+//a++;
+++a
+console.log(a);
+
+let c = 5;
+console.log(c++); // prvo se iskoristi stara vrednost promenljive pa se se onda vrednost promenljive;
+console.log(c);
+
+let d = 3;
+console.log(++d);// prvo se vredmnost promenljive poveca za 1 a onda se nova vrednost koristi u izrazu
+console.log(d);
+
+c = 4;
+d = 3;
+x = (c++) * (--d); // 4 * 2 = 8
+console.log(c,d,x); // a ovde ima c = 5 d = 2 x = 8
+
+console.log(7%2); // rezultat je 1
+console.log(9%2);
+
+console.log(4 ** 3);
+c = 3;
+d = (c ** 3)+1;
+console.log(d);
+
+// vezbe
+
+let vremeSati = 5, vremeMinuti = 30;
+
+let ponoc = 12*60;
+console.log(ponoc);
+
+let ostatakPoslePonoci = ponoc-(vremeSati*60 + vremeMinuti);
+console.log(ostatakPoslePonoci);
+
+let kolikoSatiH = ostatakPoslePonoci%12;
+console.log(kolikoSatiH);
+
+let kolikoMinutaM = ostatakPoslePonoci%60;
+console.log(kolikoMinutaM);
+console.log(`ostalo do ponoci ${kolikoSatiH}h i ${kolikoMinutaM}min`)
+
+
+
+
+
+
---
+++ 09_JS_OOSNOVE/zadaci.js
@@ -0,0 +1,75 @@
+// Zadatak 1
+
+let s = 15, m = 21;
+
+
+let odPonoci = s*60 + m; 
+console.log("Od ponoci je proslo " + odPonoci + " minuta");
+
+let doPonoci = 24 * 60 - odPonoci;
+console.log("do ponoci ima jos " + doPonoci + " min");
+
+
+//Zadatak 2
+
+let minOdPonoci = 909;
+let minSada = minOdPonoci % 60;
+let satSada = (minOdPonoci - minSada) / 60;
+
+console.log("Sada je " + satSada + ":" + minSada);
+
+// Drugi nacin
+
+// console.log(minOdPonoci / 60); // Deljenje daje realan broj
+// console.log(Math.floor(minOdPonoci/60));// daje ceo broj maanji ili jednak od rezultata
+// console.log(Math.ceil(minOdPonoci/60)); // daaje ceo broj veci ili jednak od rezultata
+// console.log(Math.round(minOdPonoci/60)); // daje ceo broj rezultat zaokruzivanja
+
+let satSada2 = Math.floor(minOdPonoci/60);
+let minSada2 = minOdPonoci % 60;
+
+console.log("sada je " + satSada2 + ":" + minSada2);
+
+
+//Zadataak 3
+
+let cenaRobe = 1750, novcanica = 2000;
+
+let kusur = novcanica - cenaRobe;
+
+console.log("kusur koji treba da se vraati je " + kusur + " dinara");
+
+// zadatak 6
+
+let ukupnoEu = 950;
+let dinar = 1;
+let kursEuro = 118;
+let kursDolar = 100;
+let euroDinar = kursEuro * dinar;
+
+let razmenaEuDin = kursEuro * ukupnoEu;
+
+console.log("ukupno imate " + razmenaEuDin + " dinara");
+
+let razmenaDinEu = razmenaEuDin/kursEuro;
+console.log("ukupno imate " + razmenaDinEu + " eura" );
+
+// Zadatak 7
+
+// ukupnoEU
+// dinar
+// euroDinar
+let dolarDinar = 100 * dinar;
+
+// euroDolar = 118 / 100;
+
+let euroDolar = kursEuro / kursDolar;
+
+console.log(euroDolar);
+
+let kolikoDolara = razmenaDinEu * euroDolar;
+let kolikoEura = kolikoDolara / euroDolar;
+console.log("Nakon razmene imate " + kolikoDolara + " dolara");
+console.log("Nakon razmene imate " + kolikoEura + " eura");
+
+
