fea56d7c928894919e212b5c1ddfc126e0b698ae
BokiThe <ristic1989@gmail.com>
2022-02-01 16:24:40 +0100

    getTime method in ui.js created

--- 28_CHAT_PROJECT/ui.js
+++ 28_CHAT_PROJECT/ui.js
@@ -8,8 +8,26 @@ class ChatUi {
   get elem() {
     return this._elem;
   }
+
+  getTime(date) {
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+
+    year = String(year).padStart(4, "0");
+    month = String(month).padStart(2, "0");
+    day = String(day).padStart(2, "0");
+    hour = String(hour).padStart(2, "0");
+    min = String(min).padStart(2, "0");
+
+    return `${day}.${month}.${year} - ${hour}:${min}`;
+  }
   templateLI(data) {
-    let HTMLLI = `${data.username}: ${data.message} <br/> ${data.created_at} <br/>`;
+    let HTMLLI = `${data.username}: ${data.message} <br/> ${this.getTime(
+      data.created_at.toDate()
+    )} <br/>`;
     this.elem.innerHTML += HTMLLI;
   }
 }
